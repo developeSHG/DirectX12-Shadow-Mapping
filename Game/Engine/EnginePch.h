303cfc009ed659980555931d02aa5cd2611a1a4e
developeSHG <developeSHG@gmail.com>
2023-07-07 20:26:24 +0900

    [Shadow Mapping] 렌더링 로직 수정

--- Game/Engine/EnginePch.h
+++ Game/Engine/EnginePch.h
@@ -46,18 +46,18 @@ using namespace Microsoft::WRL;
 #endif
 
 // °¢Á¾ typedef
-using int8 = __int8;
-using int16 = __int16;
-using int32 = __int32;
-using int64 = __int64;
-using uint8 = unsigned __int8;
-using uint16 = unsigned __int16;
-using uint32 = unsigned __int32;
-using uint64 = unsigned __int64;
-using Vec2 = DirectX::SimpleMath::Vector2;
-using Vec3 = DirectX::SimpleMath::Vector3;
-using Vec4 = DirectX::SimpleMath::Vector4;
-using Matrix = DirectX::SimpleMath::Matrix;
+using int8		= __int8;
+using int16		= __int16;
+using int32		= __int32;
+using int64		= __int64;
+using uint8		= unsigned __int8;
+using uint16	= unsigned __int16;
+using uint32	= unsigned __int32;
+using uint64	= unsigned __int64;
+using Vec2		= DirectX::SimpleMath::Vector2;
+using Vec3		= DirectX::SimpleMath::Vector3;
+using Vec4		= DirectX::SimpleMath::Vector4;
+using Matrix	= DirectX::SimpleMath::Matrix;
 
 enum class CBV_REGISTER : uint8
 {
